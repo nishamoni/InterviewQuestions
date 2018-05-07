e3d6d882517d4aa6d5ed421e7d1f971064728994
nishamoni <36095771+nishamoni@users.noreply.github.com>
2018-05-07 13:00:59 +0530

    Add files via upload
    for find repeating elements in 2 integer array

---
+++ RepeatElement.java
@@ -0,0 +1,22 @@
+
+import java.util.Arrays;
+import java.util.HashSet;
+public class RepeatElement {
+
+	public static void main(String[] args) {
+	
+		
+		    
+		        Integer[] i1 = {1, 2, 3, 4,7, 5, 4};
+		 
+		        Integer[] i2 = {3, 4, 5, 6, 7, 4};
+		 
+		        HashSet<Integer> set1 = new HashSet<>(Arrays.asList(i1));
+		 
+		        HashSet<Integer> set2 = new HashSet<>(Arrays.asList(i2));
+		 
+		        set1.retainAll(set2);
+		 
+		        System.out.println(set1);     //Output : [3, 4, 5]
+		    }
+		}
